4b12068b345dc517ebbf934b87eb687b5cdf2501
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-02 18:35:58 +0300

    add solution

--- task_9/the_rabbit_foot.py
+++ task_9/the_rabbit_foot.py
@@ -1,50 +1,55 @@
-#!/usr/bin/env python3
-
-
 def TheRabbitFoot(s, encode):
 
-    # вырезаю пробелы
-    s = s.replace(' ', '')
+    def encrypt(string_to_encrypt):
+        string_to_encrypt = string_to_encrypt.replace(' ', '')
 
-    # считаю длинну строки, после чего вычисляю её квадратный корень
-    s_length = len(s)
-    s_sqrt = round((s_length ** 0.5), 2)
-    print(s_length, s_sqrt)
+        s_length = len(string_to_encrypt)
+        s_sqrt = round((s_length ** 0.5), 2)
 
-    # получаем верхнюю и нижнюю границы корня, пишем их в отдельные переменные
-    # как значения колва строк и колонок
-    matrix_rows  = int(s_sqrt // 1)
-    matrix_cols = int(str(s_sqrt)[2:3])
-   
-    # наращиваем колво строк, если длинна не влезает в произведение эллементов строк*столбцов
-    while matrix_rows * matrix_cols < s_length:
-        matrix_rows += 1
-    print(matrix_rows, matrix_cols)
-    
-    
-    decipher = []
+        matrix_rows = int(s_sqrt // 1)
+        matrix_cols = int(str(s_sqrt)[2:3])
 
-    while len(s) > matrix_cols:
-        decipher.append(list(s[:matrix_cols]))
-        s = s[matrix_cols:]
+        while matrix_rows * matrix_cols < s_length:
+            matrix_rows += 1
+
+        encrypt_list = []
+
+        while len(string_to_encrypt) > matrix_cols:
+            encrypt_list.append(list(string_to_encrypt[:matrix_cols]))
+            string_to_encrypt = string_to_encrypt[matrix_cols:]
+        else:
+            encrypt_list.append(list(string_to_encrypt))
+
+        result_list = []
+
+        for i in range(matrix_cols):
+            for j in range(matrix_rows):
+                if i < len(encrypt_list[j]):
+                    result_list.append(encrypt_list[j][i])
+            result_list.append(' ')
+
+        result_list = ''.join(result_list).strip()
+
+        return result_list
+
+    def decrypt(encoded_string):
+
+        columns = encoded_string.split()
+
+        characters = []
+        index = 0
+
+        while True:
+            for column in columns:
+                try:
+                    character = column[index]
+                except IndexError:
+                    return ''.join(characters)
+                characters.append(character)
+            index += 1
+
+    if encode:
+        encrypt(s)
     else:
-        decipher.append(list(s))
-        
-    result = []
-         
-    for i in range(matrix_cols):
-        for j in range(matrix_rows):
-            if i < len(decipher[j]):
-                result.append(decipher[j][i])
-        result.append(' ')
-                
-    result = ''.join(result).strip()
-    
-    print(result)
+        decrypt(s)
 
-
-
-
-
-
-TheRabbitFoot('отдай мою кроличью лапку', True)
