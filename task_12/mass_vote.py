34d8962533ff6149b0beec4055334638b3c04a4a
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-05 22:50:42 +0300

    change solution

--- task_12/mass_vote.py
+++ task_12/mass_vote.py
@@ -8,15 +8,22 @@ def MassVote(N, Votes):
         vote_amount = round(vote_amount, 3)
         percents.append(vote_amount)
 
-    set_percent = set(percents)
-
-    if len(percents) != len(set_percent):
-        result = 'no winner'
-
+    result = 'no winner'
     res_max = max(percents)
+
+    def check_draw(max_vote, vote_percents):
+        counter = 0
+        draw_result = False
+        for item in vote_percents:
+            if item == max_vote:
+                counter += 1
+        if counter > 1:
+            draw_result = True
+        return draw_result
+
     if res_max > 50.0:
         result = 'majority winner ' + str(percents.index(res_max) + 1)
-    elif res_max <= 50.0 and len(percents) == len(set_percent):
+    elif res_max <= 50.0 and not check_draw(res_max, percents):
         result = 'minority winner ' + str(percents.index(res_max) + 1)
 
     return result
