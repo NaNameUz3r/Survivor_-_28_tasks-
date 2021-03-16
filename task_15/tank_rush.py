23a347dae5157c50b2fba7e1080233c1d75c9c3a
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-16 16:25:37 +0300

    update solution logic

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -1,14 +1,17 @@
 def TankRush(H1, W1, S1, H2, W2, S2):
-
     first_map = S1.split()
     second_map = S2.split()
 
     result = False
-    
+    founding = []
     for i in second_map:
         for j in first_map:
             if i in j:
-                result = True
-                break
+                founding.append(j.find(i[0]))
+
+    for g in range(len(founding) - 1):
+        if founding[g] == founding[g + 1]:
+            result = True
+            break
     return result
 
