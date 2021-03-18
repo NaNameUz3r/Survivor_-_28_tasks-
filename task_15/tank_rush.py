975e4d8dca5443af5ddea1ccad53835fd948b550
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-18 17:35:07 +0300

    update solution code. Add index check for nested loop

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -6,6 +6,8 @@ def TankRush(H1, W1, S1, H2, W2, S2):
         if second_map[0] in first_map[i] and i < len(first_map) - 1:
             position = first_map[i].find(second_map[0][0])
             for j in range(H2):
+                if j >= len(first_map) - 1:
+                    break
                 if second_map[j] in first_map[i + j] and (
                         first_map[i + j].find(second_map[j][0]) == position
                 ):
