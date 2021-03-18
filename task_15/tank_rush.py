ce4c85dc36feb17882733c77f64fc7e93bcd772f
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-18 18:26:02 +0300

    delete comented old check from colution. Fix mistake in import in tests for same task

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -6,7 +6,6 @@ def TankRush(H1, W1, S1, H2, W2, S2):
         if second_map[0] in first_map[i] and i < H1 - 1:
             position = first_map[i].find(second_map[0][0])
             for j in range(H2):
-                # if j > H1 - 1:
                 if (H2 - 1) > (H1 - 1 - i):
                     break
                 if second_map[j] in first_map[i + j] and (
