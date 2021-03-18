e806f03c9de961109e101c9f2a1a7e4547e31c64
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-18 15:13:04 +0300

    add new test in task_15_test.py no new errors found

--- task_15_tests.py
+++ task_15_tests.py
@@ -11,7 +11,7 @@ class TankRushTest(unittest.TestCase):
     def test2(self):
         self.assertEqual(tank_rush.TankRush(3, 4, "1234 2345 0987", 2, 2, "23 98"), False)
 
-    def test_random(self):
+    def test_random_true(self):
         first_map = ''
         second_map = ''
 
@@ -26,6 +26,22 @@ class TankRushTest(unittest.TestCase):
                 second_map += ' '
 
         self.assertEqual(tank_rush.TankRush(10000, 7, first_map, 2, 2, second_map), True)
+
+    def test_random_false(self):
+        first_map = ''
+        second_map = ''
+
+        for d in range(10000):
+            first_map += str(random.randint(1000000, 9999999))
+            if d < 10000:
+                first_map += ' '
+
+        for b in range(1000):
+            second_map += str(random.randint(10, 99))
+            if b < 1000:
+                second_map += ' '
+
+        self.assertEqual(tank_rush.TankRush(10000, 7, first_map, 1000, 2, second_map), False)
 
 
 if __name__ == '__main__':
