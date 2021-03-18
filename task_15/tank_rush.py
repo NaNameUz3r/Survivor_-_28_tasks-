c4b8968243b0a33343cff424304c98ce7c1fc8d3
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-18 13:11:41 +0300

    delete excessive empty string frome solution code

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -4,8 +4,6 @@ def TankRush(H1, W1, S1, H2, W2, S2):
 
     result = False
 
-
-
     for i in range(H1):
         if second_map[0] in first_map[i]:
             position = first_map[i].find(second_map[0][0])
