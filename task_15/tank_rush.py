39b762b27f23d485e1d0f4a4a9d4758ecd662f2d
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-03-21 10:42:33 +0300

    completely refactored task_15 code. Code without execessive and usless in our case builtin functions. Now the program should run through all server tests.

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -1,21 +1,25 @@
 def TankRush(H1, W1, S1, H2, W2, S2):
-    first_map = S1.split()
-    second_map = S2.split()
 
-    for i in range(H1):
-        if second_map[0] in first_map[i]:
-            position = first_map[i].find(second_map[0])
-            for j in range(H2):
-                if (H2 - 1) > (H1 - 1 - i):
-                    break
-                if second_map[j] in first_map[i + j] and (
-                        first_map[i + j].find(second_map[j]) == position
-                ):
-                    continue
-                else:
-                    break
-            else:
-                return True
+    field = S1.split()
+    to_find = S2.split()
 
-    return False
+    global_match_flag = False
+
+    for window_y in range(len(field) - len(to_find) + 1):
+
+        if global_match_flag:
+            break
+
+        for window_x in range(len(field[0]) - len(to_find[0]) + 1):
+            match_flag = True
+            for find_y in range(len(to_find)):
+                for find_x in range(len(to_find[0])):
+                    if field[window_y + find_y][window_x + find_x] != to_find[find_y][find_x]:
+                        match_flag = False
+
+            if match_flag:
+                global_match_flag = True
+                break
+
+    return global_match_flag
 
