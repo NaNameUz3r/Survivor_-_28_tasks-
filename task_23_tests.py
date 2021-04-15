e34d177b0a19a6403642adca8d4dce2c14d5611f
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-04-15 10:46:46 +0300

    add task_23 regression test

---
+++ task_23_tests.py
@@ -0,0 +1,24 @@
+import unittest
+from task_23 import tree_of_life
+
+
+class TankRushTest(unittest.TestCase):
+
+    def test_regression(self):
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 1, [".+..", "..+.", ".+.."]), [".+..", "..+.", ".+.."])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 2, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 3, [".+..", "..+.", ".+.."]), ['...+', '+...', '...+'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 4, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 5, [".+..", "..+.", ".+.."]), ['.+..', '..+.', '.+..'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 6, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 7, [".+..", "..+.", ".+.."]), ['...+', '+...', '...+'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 8, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 9, [".+..", "..+.", ".+.."]), ['.+..', '..+.', '.+..'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 10, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 11, [".+..", "..+.", ".+.."]), ['...+', '+...', '...+'])
+        self.assertEqual(tree_of_life.TreeOfLife(3, 4, 12, [".+..", "..+.", ".+.."]), ['++++', '++++', '++++'])
+
+
+if __name__ == '__main__':
+    unittest.main()
+
