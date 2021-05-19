85d6dd5d9b54365654aa97d8033589169484ccf8
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-05-19 22:24:18 +0300

    refactored 26 task

--- task_26/balanced_parentheses.py
+++ task_26/balanced_parentheses.py
@@ -2,29 +2,30 @@ def build_balance(build_variant,
                   open_parentheses,
                   close_parentheses,
                   init_opened_amount,
-                  result):
+                  current_results_list):
 
     if open_parentheses == init_opened_amount and (
             close_parentheses == init_opened_amount):
-        result.append(build_variant)
+        current_results_list.append(build_variant)
     else:
         if open_parentheses < init_opened_amount:
             build_balance(build_variant + '(',
                           open_parentheses + 1,
                           close_parentheses,
                           init_opened_amount,
-                          result)
+                          current_results_list)
         if close_parentheses < open_parentheses:
             build_balance(build_variant + ')',
                           open_parentheses,
                           close_parentheses + 1,
                           init_opened_amount,
-                          result)
-    return result
+                          current_results_list)
+    return current_results_list
 
 
 def BalancedParentheses(N):
-    result = []
-    result = build_balance('', 0, 0, N, result)
-    return ' '.join(result)
+    balanced_combinations = build_balance('', 0, 0, N, [])
+    return ' '.join(balanced_combinations)
 
+x = BalancedParentheses(4)
+print(x)
