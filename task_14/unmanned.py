ac57d6316b6b4986711a65b26f3f49046e4fad74
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-06-01 12:01:23 +0300

    refactoring

--- task_14/unmanned.py
+++ task_14/unmanned.py
@@ -1,7 +1,7 @@
 def Unmanned(L, N, track):
 
     road_length = L
-    abs_time = 0
+    absolute_time = 0
     moving_time = 0
     time_gap = 0
     while road_length > 0:
@@ -11,16 +11,16 @@ def Unmanned(L, N, track):
             break
 
         if moving_time == track[0][0] and (
-                abs_time % (track[0][1] + track[0][2])) < track[0][1]:
-            time_diff = track[0][1] - (abs_time % (track[0][1] + track[0][2]))
+                absolute_time % (track[0][1] + track[0][2])) < track[0][1]:
+            time_diff = track[0][1] - (absolute_time % (track[0][1] + track[0][2]))
             time_gap += time_diff
             moving_time += time_diff
-            abs_time += time_diff
+            absolute_time += time_diff
             if len(track) > 1:
                 track[1][0] = track[1][0] + time_gap
             track.pop(0)
             continue
-        abs_time += 1
+        absolute_time += 1
         moving_time += 1
         road_length -= 1
 
