99551bd2907dd54ce23a79e29d60cd765d9623df
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-06-01 12:13:54 +0300

    refactor

--- task_16/maximun_discount.py
+++ task_16/maximun_discount.py
@@ -1,9 +1,9 @@
 def MaximumDiscount(N, price):
 
-    goods = sorted(price, reverse=True)
+    goods_prices_list = sorted(price, reverse=True)
 
-    fool_discount = sum(goods[::-1][:N // 3])
-    smart_discount = sum(goods[2::3])
+    fool_discount = sum(goods_prices_list[::-1][:N // 3])
+    smart_discount = sum(goods_prices_list[2::3])
 
     return max([fool_discount, smart_discount])
 
