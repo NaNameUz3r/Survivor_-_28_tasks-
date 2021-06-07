abe8480f348ca2a0b8f52add0e6b7ca5450c0f6b
NaNameUz3r <f4ckj00gl3@gmail.com>
2021-06-07 11:00:27 +0300

    refactored a little

--- task_15/tank_rush.py
+++ task_15/tank_rush.py
@@ -1,20 +1,20 @@
 def TankRush(H1, W1, S1, H2, W2, S2):
 
-    field = S1.split()
-    to_find = S2.split()
+    GLOBAL_FIELD = S1.split()
+    TAGRET_FIELD = S2.split()
 
-    global_found= False
+    global_found = False
 
-    for window_y in range(len(field) - len(to_find) + 1):
+    for window_y in range(len(GLOBAL_FIELD) - len(TAGRET_FIELD) + 1):
 
         if global_found:
             break
 
-        for window_x in range(len(field[0]) - len(to_find[0]) + 1):
+        for window_x in range(len(GLOBAL_FIELD[0]) - len(TAGRET_FIELD[0]) + 1):
             found = True
-            for find_y in range(len(to_find)):
-                for find_x in range(len(to_find[0])):
-                    if field[window_y + find_y][window_x + find_x] != to_find[find_y][find_x]:
+            for find_y in range(len(TAGRET_FIELD)):
+                for find_x in range(len(TAGRET_FIELD[0])):
+                    if GLOBAL_FIELD[window_y + find_y][window_x + find_x] != TAGRET_FIELD[find_y][find_x]:
                         found = False
 
             if match_flag:
